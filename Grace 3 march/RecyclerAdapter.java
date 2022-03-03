516e7d6b21e7fa7a487f8d214b474dbb44b8ecb3
chrsgk <35971605+chrsgk@users.noreply.github.com>
2022-03-03 20:12:18 +0100

    Recycled view changes

---
+++ Grace 3 march/RecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.recyclerview;
+
+        import android.support.v7.widget.CardView;
+        import android.support.v7.widget.RecyclerView;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
+
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private String[] myData;
+
+    public RecyclerAdapter(String[] data) {
+
+        myData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.textview.setText(myData[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return myData.length;
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
+
+        CardView carta;
+
+        TextView textview;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textview = (TextView) itemView.findViewById(R.id.title);
+            carta =(CardView) itemView.findViewById(R.id.card);
+
+            carta.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case R.id.card:
+
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            return false;
+        }
+    }
+}
